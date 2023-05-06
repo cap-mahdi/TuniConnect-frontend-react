3466f2df58041e973f3620a0ea2580af4c6a0cb4
Medaminegdoura <medaminegdoura@gmail.com>
2023-05-06 10:28:00 +0100

    testing realtime

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
 import './App.css';
 import Main from './pages/Main';
+import SignIn from './pages/SignIn';
+import React, { useState, useEffect } from 'react';
 function App() {
+  const [messages, setMessages] = useState([]);
+
+  useEffect(() => {
+    const socket = new WebSocket('ws://localhost:8080');
+
+    socket.onmessage = (event) => {
+      const message = event.data;
+      setMessages((messages) => [...messages, message]);
+    };
+
+    return () => {
+      // socket.close();
+    };
+  }, []);
+
+  const sendMessage = (message) => {
+    const socket = new WebSocket('ws://localhost:8080');
+    socket.onopen = () => {
+      socket.send(message);
+      // socket.close();
+    };
+  };
   return (
     <>
-      <Main />
+      <div>
+        {messages.map((message, index) => (
+          <p key={index}>{message}</p>
+        ))}
+        <form
+          onSubmit={(event) => {
+            event.preventDefault();
+            const messageInput = event.target.elements.message;
+            sendMessage(messageInput.value);
+            messageInput.value = '';
+          }}
+        >
+          <input type="text" name="message" />
+          <button type="submit">Send</button>
+        </form>
+      </div>
     </>
   );
 }
