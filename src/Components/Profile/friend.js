61cd1c54c1436eb1e85349eb769c8c412a0a3884
Medaminegdoura <medaminegdoura@gmail.com>
2023-05-09 14:46:29 +0100

    profile friends

--- src/Components/Profile/friend.js
+++ src/Components/Profile/friend.js
@@ -1,20 +1,18 @@
-import React from "react";
+import React from 'react';
 import { Link } from 'react-router-dom';
 
-
-
 const Friend = (props) => {
-
-    return (
-        <div className="bg-white p-0.5">
-        <img src={props.imageUrl}
-            className="w-24 h-24 rounded-md mt-2 cursor-pointer"
-        />
-        <Link to={`/profile/`+props.id} className="font-semibold text-sm">{props.firstName +' '+props.lastName}</Link>
+  return (
+    <div className="bg-white p-0.5">
+      <img
+        src={`http://localhost:8000/images/${props.imageUrl}`}
+        className="w-24 h-24 rounded-md mt-2 cursor-pointer"
+      />
+      <Link to={`/profile/` + props.id} className="font-semibold text-sm">
+        {props.firstName + ' ' + props.lastName}
+      </Link>
     </div>
-    );
+  );
 };
-
-
 
 export default Friend;
